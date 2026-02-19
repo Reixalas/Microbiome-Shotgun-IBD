9ec982a4fefaaf29cb3a41e525f8dc7852ca56cb
Reixalas <rubensalasreixach@gmail.com>
2026-02-19 19:52:02 +0100

    Add SLURM script for running FastQC analysis

---
+++ scripts/shotgunR.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+#SBATCH --job-name=shotgunR				# Job name
+#SBATCH --output=shotgunR.%j.out		# Name of stdout output file (%j expands to %jobId)
+#SBATCH --error=shotgunR.%j.err		# Name of stdout output file (%j expands to %jobId)
+#SBATCH --cpus-per-task=4			# Specifies that the job requires 6 CPUs
+#SBATCH --mem=32G				# Specifies that the job requires 48 gigabytes of memory.
+
+module load miniconda3
+source ~/.bashrc
+conda activate shotgun1
+
+INPUT_DIR="/cabina/digdb/MBio/IBD_Biologics_5samples/Seq"
+OUTPUT_DIR="/home/41701728z/resultatsfastqc"
+
+fastqc "$INPUT_DIR"/*_1.fastq.gz "$INPUT_DIR"/*_2.fastq.gz -o "$OUTPUT_DIR"
+
+
+
+# Wait for all background jobs to finish
+wait
+
+module purge
+echo "All tasks completed"
