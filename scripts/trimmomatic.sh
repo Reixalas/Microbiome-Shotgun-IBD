ba1088367a1ce2ba94602410b78f90f2ea5a82bb
Ruben Salas Reixach <rubensalasreixach@gmail.com>
2026-02-22 15:02:49 +0100

    Update trimmomatic.sh

--- scripts/trimmomatic.sh
+++ scripts/trimmomatic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=trimmomatic			# Job name
-#SBATCH --output=trimmomatic.%j.out		# Name of stdout output file (%j expands to %jobId)
-#SBATCH --error=trimmomatic.%j.err		# Name of stdout output file (%j expands to %jobId)
+#SBATCH --job-name=trimmomatic_R			# Job name
+#SBATCH --output=trimmomatic_R.%j.out		# Name of stdout output file (%j expands to %jobId)
+#SBATCH --error=trimmomatic_R.%j.err		# Name of stdout output file (%j expands to %jobId)
 #SBATCH --cpus-per-task=4			# Specifies that the job requires 6 CPUs
 #SBATCH --mem=32G				# Specifies that the job requires 48 gigabytes of memory.
 
