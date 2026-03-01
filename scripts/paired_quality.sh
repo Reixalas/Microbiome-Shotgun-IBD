13870c93d25b52710eb4442684629afe947e9a70
Ruben Salas Reixach <rubensalasreixach@gmail.com>
2026-03-01 23:28:18 +0100

    Update paired_quality.sh

--- scripts/paired_quality.sh
+++ scripts/paired_quality.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-#SBATCH --job-name=paired_quality_R				# Job name
-#SBATCH --output=paired_quality_R.%j.out		# Name of stdout output file (%j expands to %jobId)
-#SBATCH --error=paired_quality_R.%j.err		# Name of stdout output file (%j expands to %jobId)
+#SBATCH --job-name=paired_qualitat				# Job name
+#SBATCH --output=paired_qualitat.%j.out		# Name of stdout output file (%j expands to %jobId)
+#SBATCH --error=paired_qualitat.%j.err		# Name of stdout output file (%j expands to %jobId)
 #SBATCH --cpus-per-task=4			# Specifies that the job requires 6 CPUs
 #SBATCH --mem=32G				# Specifies that the job requires 48 gigabytes of memory.
 
@@ -10,15 +10,12 @@ module load miniconda3
 source ~/.bashrc
 conda activate shotgun1
 
-INPUT_DIR="/home/41701728z/trimmo"
-OUTPUT_DIR="/home/41701728z/qualitat_paired"
+INPUT_DIR="/cabina/digdb/MBio/IBD_Biologics/trimmomatic_seq"
+OUTPUT_DIR="/cabina/digdb/MBio/IBD_Biologics/quality_paired_seq"
+mkdir -p "$OUTPUT_DIR"
 
-fastqc "$INPUT_DIR"/*_R1_paired.fastq.gz "$INPUT_DIR"/*_R2_paired.fastq.gz -o "$OUTPUT_DIR"
+fastqc -t 4 "$INPUT_DIR"/*_R1_paired.fastq.gz "$INPUT_DIR"/*_R2_paired.fastq.gz -o "$OUTPUT_DIR"
 
-
-
-# Wait for all background jobs to finish
-wait
 
 module purge
 echo "All tasks completed"
